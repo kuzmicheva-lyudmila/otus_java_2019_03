e7a7bc16f865bd87d92802193c731cf926ed693e
kuzmicheva-lyudmila <kuzmicheva.lyudmila@gmail.com>
2019-04-21 03:05:08 +0700

    edit main

---
+++ hw01-maven/src/main/java/HelloOtus.java
@@ -0,0 +1,14 @@
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+        String[] words = new String[] {"one", "one", "two", "three"};
+        Multiset<String> counts = HashMultiset.create();
+        for (String word : words) {
+            counts.add(word);
+        }
+        System.out.println(counts.toString());
+    }
+}
